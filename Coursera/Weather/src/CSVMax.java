82fa42735e5c7b55c3af7be8ce74b0670c2f8af1
abhijithraok <abhijithraok@gmail.com>
2017-05-19 19:00:08 +0530

    ColdestHourInFile

--- Coursera/Weather/src/CSVMax.java
+++ Coursera/Weather/src/CSVMax.java
@@ -5,7 +5,7 @@ import edu.duke.*;
 import org.apache.commons.csv.*;
 
 public class CSVMax {
-    public CSVRecord ColdestHourInFile(CSVParser Parser) {
+    public CSVRecord HottestHourInFile(CSVParser Parser) {
         CSVRecord LargestSoFar = null;
         for (CSVRecord CurrentRow : Parser) {
             if (LargestSoFar == null) {
@@ -22,10 +22,32 @@ public class CSVMax {
         return LargestSoFar;
 
     }
+    public CSVRecord ColdestHourInFile(CSVParser Parser) {
+        CSVRecord ColdestSoFar =null;
+        for(CSVRecord CurrentRow :Parser){
+            if(ColdestSoFar == null){
+                ColdestSoFar = CurrentRow;
+            }
+            else{
+                double CurrentTemp = Double.parseDouble(CurrentRow.get("TemperatureF"));
+                double ColdestTemp = Double.parseDouble(ColdestSoFar.get("TemperatureF"));
+                if (ColdestTemp > CurrentTemp){
+                    ColdestSoFar = CurrentRow;
+                }
+            }
+        }
+        return ColdestSoFar;
+    }
+
+
+
     public void Test(){
-        FileResource fr = new FileResource("C:\\Users\\abhij\\Downloads\\nc_weather\\nc_weather\\2015\\weather-2015-01-01.csv");
-        CSVRecord Largest = ColdestHourInFile(fr.getCSVParser());
+        FileResource fr1 = new FileResource("C:\\Users\\abhij\\Downloads\\nc_weather\\nc_weather\\2015\\weather-2015-01-01.csv");
+        CSVRecord Largest = HottestHourInFile(fr1.getCSVParser());
         System.out.println("Hottest temperature was " + Largest.get("TemperatureF") + " at" + Largest.get("TimeEST"));
+        FileResource fr2 = new FileResource("C:\\Users\\abhij\\Downloads\\nc_weather\\nc_weather\\2015\\weather-2015-01-01.csv");
+        CSVRecord Coldest = ColdestHourInFile(fr2.getCSVParser());
+        System.out.println("Coldest temperature was " + Coldest.get("TemperatureF") + " at" + Coldest.get("TimeEST"));
     }
     }
 

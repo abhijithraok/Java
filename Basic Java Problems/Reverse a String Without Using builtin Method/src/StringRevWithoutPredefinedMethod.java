c49a02ed0d49153c151ba2ee12d51b931dd459f6
Abhijith Rao <abijithraok@gmail.com>
2017-06-05 22:10:03 +0530

    added palindrome method

--- Basic Java Problems/Reverse a String Without Using builtin Method/src/StringRevWithoutPredefinedMethod.java
+++ Basic Java Problems/Reverse a String Without Using builtin Method/src/StringRevWithoutPredefinedMethod.java
@@ -5,13 +5,27 @@ import java.util.Scanner;
  */
 public class StringRevWithoutPredefinedMethod {
     public static void main(String[] args) {
-        StringBuilder output = new StringBuilder();
+        StringRevWithoutPredefinedMethod a = new StringRevWithoutPredefinedMethod();
         System.out.println("Enter a string");
         Scanner scan = new Scanner(System.in);
         String input = scan.nextLine();
+        String palindrome = a.palindrome(input);
+        System.out.print("Reverse of input: " + input +" is : " + " " +palindrome);
+    }
+
+    public String palindrome(String input) {
+        StringBuilder output = new StringBuilder();
         for (int i = input.length() - 1; i >= 0; i--) {
             output.append(input.charAt(i));
         }
-        System.out.println(output);
+       String reverse= output.toString();
+        if (input.equalsIgnoreCase(reverse)) {
+            System.out.println("given input is palindrome ");
+        } else {
+            System.out.println("not a palindrome");
+        }
+
+
+        return reverse;
     }
 }

5e38cfaa209108655e7773f45011a0d7bb810ec1
Abhijith Rao <abijithraok@gmail.com>
2017-06-06 18:29:51 +0530

    added method to find sum of even fib

--- Basic Java Problems/fibonacci series/src/Fib.java
+++ Basic Java Problems/fibonacci series/src/Fib.java
@@ -1,3 +1,6 @@
+/**
+ * Created by abhij on 06-Jun-17.
+ */
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -7,23 +10,26 @@ import java.util.Scanner;
  */
 public class Fib {
     public static void main(String[] args) {
-        int f1 =0, f2 = 1, fr = 0;
+       long sum = 0;
+       long f1 =0,f2 = 1 , fr = 0;
         System.out.println("enter fn");
         Scanner input = new Scanner(System.in);
         int fn = input.nextInt();
         System.out.println("enter first number either 0 or 1");
         Scanner firstNumber = new Scanner(System.in);
-        f1 = firstNumber.nextInt();
-        ArrayList<Integer> fib = new ArrayList<Integer>();
+        f1 = firstNumber.nextLong();
+        ArrayList<Long> fib = new ArrayList<Long>();
+
+
         if (f1 == 0) {
-            for (int i = 0; i < fn; i++) {
+            for (long i = 0; i < fn; i++) {
                 fr = f1 + f2;
                 fib.add(f1);
                 f1 = f2;
                 f2 = fr;
             }
         } else if (f1 == 1) {
-            for (int i = 0; i < fn; i++) {
+            for (long i = 0; i < fn; i++) {
                 fr = f1 + f2;
                 fib.add(f1);
                 f1 = f2;
@@ -34,12 +40,21 @@ public class Fib {
         else {
             System.out.println("error");
         }
-        System.out.print(fib);
+        // for finding sum of even Fibonacci
+        for(Long i : fib){
+            if(i % 2 == 0){
+                sum += i;
+            }
+        }
+
+
+        System.out.println(fib);
+        System.out.println("Sum of even fibonacci series is " + sum);
 
     }
 }
 
- /*  public static ArrayList Fibonacci(int f1 ){
+ /*public  ArrayList Fibonacci(int f1 ){
         int f2 =1,fr=0,s,t;
        System.out.println("enter fn");
        Scanner input = new Scanner(System.in);
@@ -54,7 +69,6 @@ public class Fib {
        return Fibonacci(fr);
 
 
-    }
+    }*/
 
-}
-*/
+

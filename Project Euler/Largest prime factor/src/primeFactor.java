d687e93664ddfd1172bdc4b7078dc4dafe7af041
Abhijith Rao <abijithraok@gmail.com>
2017-06-06 22:37:58 +0530

    primefactor

--- Project Euler/Largest prime factor/src/primeFactor.java
+++ Project Euler/Largest prime factor/src/primeFactor.java
@@ -1,25 +1,40 @@
-import java.util.Scanner;
+import java.util. *;
+
 
 /**
  * Created by abhij on 06-Jun-17.
  */
 public class primeFactor {
-    public static void main(String [] args){
-        primeFactor a = new primeFactor();
-        System.out.println("Enter a Integer");
+    public static void main(String[] args) {
+        boolean number;
+        ArrayList<Long> primeNumbers = new ArrayList<Long>();
+        ArrayList<Long> primeFactor = new ArrayList<>();
+        System.out.println("enter Integer ");
         Scanner scan = new Scanner(System.in);
-        int input = scan.nextInt();
-        int prime = a.isPrime(input);
-    }
-    public int isPrime(int input){
-        for(int i = 2; i < input;i++){
-            if(i % input != 0){
-
-
+        long input = scan.nextLong();
+        primeNumbers.add((long)1);
+        for (long i = 2; i < input; i++) {
+            number =IsPrime(i);
+            if (number)
+                primeNumbers.add(i);
+        }
+        System.out.println(primeNumbers);
+        for(long prime : primeNumbers){
+            if(input % prime == 0){
+                primeFactor.add(prime);
 
             }
-        }
 
-        return 1;
+        }
+        System.out.println("Largest PrimeFactor is "+ Collections.max(primeFactor));
+    }
+
+    public static boolean IsPrime(long k) {
+        for (long i = 2; i < k; i++) {
+            if (k % i == 0)
+                return false;
+        }
+        return true;
+
     }
 }

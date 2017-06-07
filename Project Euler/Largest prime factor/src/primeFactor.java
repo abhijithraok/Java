59ce51292a465812a1d0906df884d9a209688b29
Abhijith Rao <abijithraok@gmail.com>
2017-06-07 22:21:55 +0530

    added question details in comment

--- Project Euler/Largest prime factor/src/primeFactor.java
+++ Project Euler/Largest prime factor/src/primeFactor.java
@@ -2,7 +2,12 @@ import java.util. *;
 
 
 /**
- * Created by abhij on 06-Jun-17.
+ * Created by abhijith on 06-Jun-17.
+ * Largest prime factor
+ Problem 3
+ The prime factors of 13195 are 5, 7, 13 and 29.
+
+ What is the largest prime factor of the number 600851475143 ?
  */
 public class primeFactor {
     public static void main(String[] args) {
--- Project Euler/Multiples of 3 and 5/src/multiples.java
+++ Project Euler/Multiples of 3 and 5/src/multiples.java
@@ -1,8 +1,12 @@
 import java.util.Scanner;
 
 /**
- * Created by abhij on 06-Jun-17.
- * To Find the sum of all the multiples of int1 or int2 below n
+ * Created by abhijith on 06-Jun-17.
+ * Multiples of 3 and 5
+ Problem 1
+ If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+ Find the sum of all the multiples of 3 or 5 below 1000.
  */
 
 public class multiples {

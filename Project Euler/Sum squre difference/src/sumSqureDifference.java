f5ee106f429b4e4523120f3f2a9b40be80fdddeb
Abhijith Rao <abijithraok@gmail.com>
2017-06-09 18:43:58 +0530

    sum squre difference

--- Project Euler/Sum squre difference/src/sumSqureDifference.java
+++ Project Euler/Sum squre difference/src/sumSqureDifference.java
@@ -1,13 +1,31 @@
 /**
  * Created by Abhijith on 09-Jun-17.
+ * Sum square difference
+ Problem 6
+ The sum of the squares of the first ten natural numbers is,
+
+ 12 + 22 + ... + 102 = 385
+ The square of the sum of the first ten natural numbers is,
+
+ (1 + 2 + ... + 10)2 = 552 = 3025
+ Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
+
+ Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
  */
 public class sumSqureDifference {
     public static void main(String [] args){
-        int count = 0;
+        int count1 = 0;
+        int count2 = 0;
         for (int i = 1 ; i <=100; i++){
            int j = i*i ;
-            count +=j;
+            count1 +=j;
         }
-        for(int i = 1 ; i <=100; i++)
+        for(int i = 1 ; i <=100; i++){
+            count2 += i;
+
+        }
+        count2 = count2 *count2;
+        int difference = count2 - count1;
+        System.out.println("Sum of square difference is " + difference);
     }
 }

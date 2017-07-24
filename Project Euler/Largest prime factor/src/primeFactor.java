5a33b691d803b83b3c7d382eb1518965340319bb
Abhijith Rao <abijithraok@gmail.com>
2017-07-24 19:50:10 +0530

    removed isprime method

--- Project Euler/Largest prime factor/src/primeFactor.java
+++ Project Euler/Largest prime factor/src/primeFactor.java
@@ -12,33 +12,17 @@ import java.util. *;
 public class primeFactor {
     public static void main(String[] args) {
         boolean number;
-        ArrayList<Long> primeNumbers = new ArrayList<Long>();
-        ArrayList<Long> primeFactor = new ArrayList<>();
+
         System.out.println("enter Integer ");
         Scanner scan = new Scanner(System.in);
         long input = scan.nextLong();
-        primeNumbers.add((long)1);
+
         long largestPrimeFactor = largestPrimeFactor(input);
         System.out.println("Largest Prime Factor is "+ largestPrimeFactor);
-        for (long i = 2; i < input; i++) {
-            number =IsPrime(i);
-            if (number)
-                primeNumbers.add(i);
-        }
-        System.out.println(primeNumbers);
 
 
     }
 
-    public static boolean IsPrime(long k) {
-        for (long i = 2; i < k; i++) {
-            if (k % i == 0)
-                return false;
-        }
-        return true;
-
-
-    }
     public static long largestPrimeFactor(long input){
         long i =2;
         while(input > 1){
---
+++ Project Euler/Number letter counts/src/numberLetterCounts.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Abhijith on 21-Jul-17.
+ *
+ * If the numbers 1 to 5 are written out in words: one, two, three, four, five, then there are 3 + 3 + 5 + 4 + 4 = 19 letters used in total.
+ If all the numbers from 1 to 1000 (one thousand) inclusive were written out in words, how many letters would be used?
+ */
+public class numberLetterCounts {
+    public static void main(String [] args){
+
+    }
+}

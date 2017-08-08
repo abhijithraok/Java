f101a4d93d5709c145a99236cbbc34ddce5bc29c
Abhijith Rao <abijithraok@gmail.com>
2017-08-08 22:41:23 +0530

    removed redundant code

--- Project Euler/fibonacci series/src/Fib.java
+++ Project Euler/fibonacci series/src/Fib.java
@@ -9,55 +9,22 @@
  By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.
  */
 
-import java.util.ArrayList;
-import java.util.Scanner;
-
-/**
- * Created by abhij on 09-May-17.
- */
 public class Fib {
     public static void main(String[] args) {
+        long limit = 50;
         long sum = 0;
-        long f1 = 0, f2 = 1, fr = 0;
-        System.out.println("enter fn");
-        Scanner input = new Scanner(System.in);
-        int fn = input.nextInt();
-        System.out.println("enter first number either 0 or 1");
-        Scanner firstNumber = new Scanner(System.in);
-        f1 = firstNumber.nextLong();
-        ArrayList<Long> fib = new ArrayList<Long>();
-
-
-        if (f1 == 0) {
-            for (long i = 0; i < fn; i++) {
-                fr = f1 + f2;
-                fib.add(f1);
-                f1 = f2;
-                f2 = fr;
-            }
-        } else if (f1 == 1) {
-            for (long i = 0; i < fn; i++) {
-                fr = f1 + f2;
-                fib.add(f1);
-                f1 = f2;
-                f2 = fr;
+        long f1 = 0;
+        long f2 = 1;
+        for (long i = 1; i <= limit; i++) {
+            f1 = f1 + f2;
+            f2 = f1 - f2;
+            //  System.out.println(f1);
+            if (f1 % 2 == 0 && f1 < 4000000) {
+                sum += f1;
             }
 
-        } else {
-            System.out.println("error");
+
         }
-        // for finding sum of even Fibonacci
-
-        //Even Fibonacci numbers Project euler.net #2
-
-        for (int i = 1; i < fib.size(); i++) {
-            if (fib.get(i) % 2 == 0 && fib.get(i) < 4000000) {
-                sum += fib.get(i);
-            }
-        }
-
-
-        System.out.println(fib);
         System.out.println("Sum of even fibonacci series is " + sum);
 
     }

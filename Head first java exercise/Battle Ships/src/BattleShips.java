563d6b6fdd14d0bbb426fc83ca2d73724e12e903
Abhijith Rao <abijithraok@gmail.com>
2017-11-12 19:02:45 +0530

    added pc turn

--- Head first java exercise/Battle Ships/src/BattleShips.java
+++ Head first java exercise/Battle Ships/src/BattleShips.java
@@ -7,22 +7,40 @@ import java.util.*;
 public class BattleShips {
     //keeps track of player and computers ship
     public static String[][] mainOceanMap = new String[10][10];
+    public static String[][] oceanMapComputerMiss = new String[10][10];
+    public static String[][] oceanMapUserPcInput = new String[10][10];
+
 
     public static void main(String[] args) {
-        int playerCount = 0;
-        int pcCount = 0;
+        boolean boolUser;
+        boolean boolPC;
+        int playerCount = 5;
+        int pcCount = 5;
         String[][] oceanMap = new String[10][10];
+        String[][] oceanMapTemp = new String[10][10];
         System.out.println("**** Welcome to Battle Ships game ****\n Right now, the sea is empty");
+        //display oceanMap
         oceanMapDisplay(oceanMap);
         String[][] oceanMap1 = deployPlayersShips(oceanMap);
-        String [][] oceanMap2 = deployComputersShips(oceanMap1);
+        String[][] oceanMap2 = deployComputersShips(oceanMap1);
         mainOceanMap = oceanMap2;
-        // System.out.println(Arrays.deepToString(oceanMap));
-        while (pcCount <= 2) {
-            playersTurn(mainOceanMap);
-            pcCount++;
-        }
+        while (pcCount > 0 && playerCount > 0) {
+            boolUser = playersTurn(mainOceanMap);
+            if (boolUser) {
+                playerCount--;
+            }
+            boolPC = computersTurn(mainOceanMap);
+            if (boolPC) {
+                pcCount--;
+            }
+            System.out.println("Your ships :" + playerCount + " Computer Ships : " + pcCount);
 
+        }
+        if (pcCount == 0) {
+            System.out.println("Hooray! you win the battle :)");
+        } else {
+            System.out.println("you lose");
+        }
     }
 
     //displays oseanMap 10x10
@@ -45,26 +63,28 @@ public class BattleShips {
     public static String[][] deployPlayersShips(String[][] oceanMapArray) {
         int count = 1;
         Scanner input = new Scanner(System.in);
-        while (count <= 2) {
+        while (count <= 5) {
             System.out.print("Enter X coordinate for your" + count + " ship: ");
             int x = Integer.parseInt(input.nextLine());
             System.out.print("Enter Y coordinate for your" + count + " ship: ");
             int y = Integer.parseInt(input.nextLine());
             if (x < 10 && y < 10 && oceanMapArray[y][x] == null) {
                 oceanMapArray[y][x] = "@";
+                oceanMapUserPcInput[y][x] = "@";
                 count++;
             }
 
         }
-        oceanMapDisplay(oceanMapArray);
+        oceanMapDisplay(oceanMapUserPcInput);
         return oceanMapArray;
     }
+
 
     public static String[][] deployComputersShips(String[][] oceanMapArray) {
         int count = 1;
         Random rand = new Random();
         System.out.println("Computer is deploying ships");
-        while (count <= 2) {
+        while (count <= 5) {
             int x = rand.nextInt(10);
             int y = rand.nextInt(10);
             if (oceanMapArray[y][x] == null) {
@@ -74,50 +94,83 @@ public class BattleShips {
             }
 
         }
-      //  oceanMap(oceanMapArray);
+        //  oceanMap(oceanMapArray);
         return oceanMapArray;
     }
-public static int playersTurn(String [][] oceanMap){
+
+    public static boolean playersTurn(String[][] oceanMap) {
         Scanner input = new Scanner(System.in);
         System.out.println("YOUR TURN");
         int count = 0;
-        int playerShipCount = 0;
-        while (count <= 1){
+        boolean bool = false;
+        while (count < 1) {
             System.out.println("Enter X coordinate:");
             int x = Integer.parseInt(input.nextLine());
             System.out.println("Enter Y coordinate");
             int y = Integer.parseInt(input.nextLine());
-            if(x < 10 && y < 10){
-                if(oceanMap[y][x] == null){
-                    oceanMap[y][x] ="-";
+            if (x < 10 && y < 10) {
+                if (oceanMap[y][x] == null) {
+                    oceanMap[y][x] = "-";
+                    oceanMapUserPcInput[y][x] = "-";
                     System.out.println("You missed");
-                   // oceanMapDisplay(oceanMap);
-                }
-               else if(oceanMap[y][x] == "@"){
+                    count++;
+                    // oceanMapDisplay(oceanMap);
+                } else if (oceanMap[y][x] == "@") {
                     oceanMap[y][x] = "X";
-                    System.out.println( "Oh no, you sunk your own ship :(");
+                    oceanMapUserPcInput[y][x] = "X";
+                    System.out.println("Oh no, you sunk your own ship :(");
+                    bool = true;
+                    count++;
+                } else if (oceanMap[y][x] == "#") {
+                    oceanMap[y][x] = "!";
+                    oceanMapUserPcInput[y][x] = "!";
+                    System.out.println("Boom! You Sunk the ship!");
+                    // playerShipCount++;
+                    bool = true;
+                    count++;
                 }
-                else if(oceanMap[y][x] == "#"){
-                   oceanMap[y][x] = "!";
-                   System.out.println("Boom! You Sunk the ship!");
-                   playerShipCount++;
-                }
-                count++;
+
             }
         }
-            mainOceanMap = oceanMap;
-        oceanMapDisplay(oceanMap);
-        
-     return playerShipCount;
-}
-public static int computersTurn(String[][] oceanMap){
-    int pcCount = 0;
-    int count = 0;
-    System.out.println("COMPUTER'S TURN");
-    Random rand = new Random();
-    int x =rand.nextInt(10);
-    int y = rand.nextInt(10);
-    Scanner input = new Scanner(System.in);
-    return pcCount;
-}
+        oceanMapDisplay(oceanMapUserPcInput);
+        mainOceanMap = oceanMap;
+
+
+        return bool;
+    }
+
+    public static boolean computersTurn(String[][] oceanMap) {
+        int count = 0;
+        boolean bool = false;
+        System.out.println("COMPUTER'S TURN");
+        oceanMapComputerMiss = oceanMap;
+        Random rand = new Random();
+
+        while (count < 1) {
+            int x = rand.nextInt(10);
+            int y = rand.nextInt(10);
+            if (oceanMapComputerMiss[y][x] == null) {
+                oceanMapComputerMiss[y][x] = "-";
+                System.out.println("Computer missed");
+                count++;
+                // oceanMapDisplay(oceanMap);
+            } else if (oceanMap[y][x] == "#") {
+                oceanMap[y][x] = "!";
+                oceanMapUserPcInput[y][x] = "!";
+                System.out.println("The Computer sunk one of its own ships");
+                bool = true;
+                count++;
+            } else if (oceanMap[y][x] == "@") {
+                oceanMap[y][x] = "X";
+                oceanMapUserPcInput[y][x] = "X";
+                System.out.println("The Computer sunk one of your ships!");
+                count++;
+
+                bool = true;
+            }
+
+        }
+        oceanMapDisplay(oceanMapUserPcInput);
+        return bool;
+    }
 }

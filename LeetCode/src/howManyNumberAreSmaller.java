f105de21d6f59e58a2f8b83ca986a5e1c45eaa10
Abhijith Rao <abijithraok@gmail.com>
2021-03-17 18:28:19 +0530

    how many number are smaller

---
+++ LeetCode/src/howManyNumberAreSmaller.java
@@ -0,0 +1,16 @@
+public class howManyNumberAreSmaller {
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+        int[] answer = new int[nums.length];
+        int count;
+        for (int i = 0; i < nums.length; i++) {
+            count = 0;
+            for (int j = 0; j < nums.length; j++) {
+                if (i != j && nums[j] < nums[i])
+                    count++;
+            }
+            answer[i] = count;
+        }
+        return answer;
+    }
+
+}

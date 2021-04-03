e5eb3bcad73b4b40c0fb79a123149c8561a2a692
Abhijith Rao <abijithraok@gmail.com>
2021-04-03 20:15:26 +0530

    buddy String

---
+++ LeetCode/src/buddyString.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class buddyString {
+    public static boolean buddyStrings(String A, String B) {
+        if (A.length() != B.length()) return false;
+        Character c1 = null, c2 = null;
+        Set<Character> set = new HashSet<>();
+        int count = 0;
+        for (int i = 0; i < A.length(); i++) {
+            if (A.charAt(i) != B.charAt(i)) {
+                if (count > 2) return false;
+                if (c1 == null) {
+                    c1 = B.charAt(i);
+                    c2 = A.charAt(i);
+                    count++;
+                    continue;
+                }
+                if (c1 != A.charAt(i) || c2 != B.charAt(i)) {
+                    return false;
+                }
+                count++;
+            }
+            set.add(A.charAt(i));
+        }
+        return count == 2 || (count == 0 && set.size() < A.length());
+    }
+
+}

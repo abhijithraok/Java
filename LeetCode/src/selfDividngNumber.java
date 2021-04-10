11d0dd9c8cc852b36581aa6f48642b79637b4407
Abhijith Rao <abijithraok@gmail.com>
2021-04-10 17:21:19 +0530

    self dividing number

---
+++ LeetCode/src/selfDividngNumber.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class selfDividngNumber {
+    public static List<Integer> selfDividingNumbers(int left, int right) {
+        List<Integer> res = new ArrayList<>();
+        for (int i = left; i <= right; i++)
+            if (dividingNumber(i)) res.add(i);
+        return res;
+    }
+
+    public static boolean dividingNumber(int num) {
+        for (int n = num; n > 0; n /= 10)
+            if (n % 10 == 0 || num % (n % 10) != 0) return false;
+        return true;
+    }
+}
